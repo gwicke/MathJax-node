cd12c712fb69e01aac51c09676f485be11595a3e
physikerwelt <wiki@physikerwelt.de>
2016-01-31 12:30:36 +0100

    Add replace img with mathoidStyle
    Remove the fake img tag returned by MathJax-node
    and return the style information (customized for mathoid)
    instead

--- test/Style.js
+++ test/Style.js
@@ -2,7 +2,7 @@ var tape = require('tape');
 var mjAPI = require("..//lib/mj-single.js");
 
 tape('basic test: check MathJax core', function(t) {
-    t.plan(1);
+    t.plan(2);
 
     var tex = '\\sdot'; // Other possible test candidates \\bull, \\vline
     mjAPI.start();
@@ -10,8 +10,10 @@ tape('basic test: check MathJax core', function(t) {
     mjAPI.typeset({
         math: tex,
         format: "inline-TeX",
-        svg: true
+        svg: true,
+        mathoidStyle: true
     }, function(data) {
-        t.equal(data.svg.indexOf("margin-"),-1, 'There should be no margins');
+        t.equal(data.svg.indexOf('margin-'),-1,'There should be no margins');
+        t.ok(data.mathoidStyle.indexOf('width')>0,'Mathoid style must include width info');
     });
 });
